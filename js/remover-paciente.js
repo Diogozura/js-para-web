0748f7a2116fade2069be252f326c8eef862c891
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-10-14 08:14:46 -0300

    animação ao remover a linha do paciente
    usando setTimeOut para remover e add uma animação em css

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,25 @@
 var pacientes = document.querySelectorAll(".paciente");
 
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function (paciente) {
-    paciente.addEventListener("dblclick", function () {
-        console.log("clicou em mim fdp. vem pra porrada carai");
-        this.remove()
-    })
+tabela.addEventListener("dblclick", function (event) {
+
+    // var alvoEvento = event.target;
+    // var paiDoAlvo = alvoEvento.parentNode
+    // paiDoAlvo.remove()
+
+    event.target.parentNode.classList.add("fadeOut")
+
+    setTimeout(function () {
+        event.target.parentNode.remove()
+    },5 00)  
+
 })
+
+
+// pacientes.forEach(function (paciente) {
+//     paciente.addEventListener("dblclick", function () {
+//         console.log("clicou em mim fdp. vem pra porrada carai");
+//         this.remove()
+//     })
+// })
